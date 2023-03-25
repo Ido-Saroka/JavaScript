2d0635d4c37093a925fc6f97227f4e8602e971b1
IdoS <ido.saroka@easyr.co>
2023-03-25 14:32:26 +0300

    Added JavaScript project setup with ESlint and Prettier configuration
    * added array.js - will store various array functions

---
+++ GeneralFunctions/src/arrays.js
@@ -0,0 +1,26 @@
+function isArrayValid(arrayToTest) {
+    if (arrayToTest == null || arrayToTest.length === 0) {
+        return false;
+    }
+    return true;
+}
+
+export const filterInPlace = (array, predicate) => {
+    try {
+      //Validate received arguments
+      if (!isArrayValid(array) || predicate == null) {
+        return null;
+      }
+      let end = 0;
+      for (let i = 0; i < array.length; i += 1) {
+        const obj = array[i];
+        if (predicate(obj)) {
+          array[end++] = obj;
+        }
+      }
+      array.length = end;
+      
+    } catch (exception) {
+        console.log(`Exception while filtering array: ${exception}`)
+    }
+  };
