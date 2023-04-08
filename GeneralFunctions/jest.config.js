7ebf100b503b3f0cb86e8506515afd85e62635cc
IdoS <ido.saroka@easyr.co>
2023-04-08 16:34:23 +0300

    jest.config.js - added aliases resolve

--- GeneralFunctions/jest.config.js
+++ GeneralFunctions/jest.config.js
@@ -6,7 +6,12 @@
  */
 module.exports = {
     //Search for specified modules inside the "src" directory (resolves the error Jest failing to recognize path aliases defined inside "jsconfig.json")
-    moduleDirectories: ["node_modules", "src"],
+    moduleDirectories: ["node_modules","src"],
+
+    //Resolve "jsconfig.js" aliases so they can be used inside Jest test files 
+    moduleNameMapper: {
+      "@/(.*)": "<rootDir>/src/$1",
+    },
 
     //Only look for test files in the test directory and all subdirectories.
     testMatch: ['**/test/**/*.test.js'],
