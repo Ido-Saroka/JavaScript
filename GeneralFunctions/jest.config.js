97a37eba00481c71814d4ac06ff76887addd5080
IdoS <ido.saroka@easyr.co>
2023-04-09 07:37:27 +0300

    Added custom converter to change jsconfig,json aliases into jest recognizable regex format (avoid duplicate declarations)

--- GeneralFunctions/jest.config.js
+++ GeneralFunctions/jest.config.js
@@ -4,14 +4,51 @@
  * Additional info:  
  * - {@link https://jestjs.io/docs/configuration Jest Docs - Configuring Jest}
  */
+
+//Needed for resolving vari
+const path = require('path');
+
+
+/**
+ * @description Converts aliases specified inside "jsconfig.json" to jest supported regex format.  
+ * Function is used to avoid duplicate deceleration of aliases in both "jsconfig.json" and in "jest.config.js" 
+
+ * Additional info:
+ * - {@link https://jestjs.io/docs/configuration#modulenamemapper-objectstring-string--arraystring Jest Docs - Configuring Jest - moduleNameMapper }
+ * 
+ * @returns {Object} "jsconfig.json" aliases converted to Jest moduleNameMapper regex format.
+ * @example
+ * //will convert the provided path aliases (jsconfig.json):
+ *    "paths": {"@/*": ["src/*"]}
+ * //Into: 
+ *    {@/(.*): '<rootDir>/src/$1'}
+ */
+function getAliases() {
+  //Get the paths property info from jsconfig.json
+  const jsConfigPath = path.resolve(__dirname, 'jsconfig.json');
+  const jsConfig = require(jsConfigPath);
+  const { paths } = jsConfig.compilerOptions;
+
+  //Convert the aliases to jest supported format
+  const aliases = {};
+  for (const alias in paths) {
+    const aliasPath = paths[alias][0];
+    const aliasKey = `${alias.replace('/*', '/(.*)')}`;
+    const aliasMapping = `<rootDir>/${aliasPath.replace('/*', '/$1')}`;
+    aliases[aliasKey] = aliasMapping;
+  }
+  
+  //Return converted aliases
+  return aliases;
+}
+
+
 module.exports = {
     //Search for specified modules inside the "src" directory (resolves the error Jest failing to recognize path aliases defined inside "jsconfig.json")
     moduleDirectories: ["node_modules","src"],
 
-    //Resolve "jsconfig.js" aliases so they can be used inside Jest test files 
-    moduleNameMapper: {
-      "@/(.*)": "<rootDir>/src/$1",
-    },
+    //Resolve aliases specified in "jsconfig.json" so Jest test files can recognize them correctly
+    moduleNameMapper:   getAliases(),
 
     //Only look for test files in the test directory and all subdirectories.
     testMatch: ['**/test/**/*.test.js'],
